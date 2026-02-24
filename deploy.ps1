01c3e8abe53eb082e0ede6792176e6ff25980606
Yijia-phys <y.zhao.phys@gmail.com>
2026-02-24 21:33:36 +0800

    add page footer

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,13 @@
 quarto render
+
+$msg = Read-Host "Please enter a commit message (press Enter to use the default message)"
+
+if ([string]::IsNullOrWhiteSpace($msg)) {
+    $msg = "Update blog: $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
+}
+
 git add .
-git commit -m "Update blog: $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
+git commit -m "$msg"
 git push origin main
+
+Write-Host "Yattaze" -ForegroundColor Green
